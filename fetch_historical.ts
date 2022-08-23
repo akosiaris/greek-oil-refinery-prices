fc709696d9ae430051caa2027415456965abe9a4
Alexandros Kosiaris <akosiaris@gmail.com>
2022-08-23 12:27:27 +0300

    A somewhat more complete fetch historical script
    Not bad, not great.
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- fetch_historical.ts
+++ fetch_historical.ts
@@ -1,19 +1,35 @@
 import parse from 'https://deno.land/x/date_fns/parse/index.js';
 import { el, enUS } from "https://deno.land/x/date_fns/locale/index.js";
-import { writeTXT } from 'https://deno.land/x/flat/mod.ts';
-import { parseOilPage } from './postprocess.ts';
+import { writeTXT, readTXT } from 'https://deno.land/x/flat/mod.ts';
+import { parseOilPage, writeDataFiles } from './postprocess.ts';
 
 const site = 'http://oil.gge.gov.gr/?p=';
-const start:number=11505;
+let start:number=11505;
 let end:number=15154;
-end = 11513;
+
 for (var i:number=start; i<=end; i++) {
+  /*
   let resp = await fetch(site + i);
   if (!(resp.status == 200)) {
     continue;
   }
   let txt = await resp.text();
-  let pagedata:object = parseOilPage(txt);
-  console.log(pagedata);
-  await writeTXT('html_' + i, txt );
+  await writeTXT('old_data/html_' + i, txt );
+  */
+  let txt = null;
+  try {
+    txt = await readTXT('old_data/html_' + i);
+  } catch(error) {
+    //console.log('File html_' + i + ' does not exist');
+    continue;
+  }
+  let pagedata:[object] = parseOilPage(txt);
+  if (!pagedata) {
+    console.log('Not parsed' + txt);
+  } else if (pagedata.length > 0) {
+    writeDataFiles(pagedata);
+    continue
+  } else {
+    console.log('Empty page, not parsed? html_' + i);
+  }
 }
