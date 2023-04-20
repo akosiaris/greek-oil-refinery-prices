f4629eeda523d8b18d363239379ecece4b8084ac
Alexandros Kosiaris <akosiaris@gmail.com>
2023-04-20 15:11:29 +0300

    Move fetch_historical to utils/
    Put various utilities in there for now

---
+++ utils/fetch_historical.ts
@@ -0,0 +1,33 @@
+import { writeTXT, readTXT } from 'https://deno.land/x/flat/mod.ts';
+import { parseFuelPage, writeDataFiles } from '../postprocess.ts';
+
+const site = 'http://oil.gge.gov.gr/?p=';
+// Some sane defaults
+let start = 11505;
+let end = 15154;
+
+start = parseInt(Deno.args[0]);
+end = parseInt(Deno.args[1]);
+
+for (let i: number=start; i<=end; i++) {
+  let txt = '';
+  try {
+    txt = await readTXT('dist/html_' + i);
+  } catch(_error) {
+    let resp = await fetch(site + i);
+    if (!(resp.status == 200)) {
+      continue;
+    }
+    txt = await resp.text();
+    await writeTXT('dist/html_' + i, txt);
+  }
+  let pagedata: FuelEntry[] = parseFuelPage(txt);
+  if (!pagedata) {
+    console.log('Not parsed' + txt);
+  } else if (pagedata.length > 0) {
+    await writeDataFiles(pagedata);
+    continue
+  } else {
+    console.log('Empty page, not parsed? html_' + i);
+  }
+}
