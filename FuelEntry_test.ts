88f4251d08c81dc3a3d5cd1aeb6ad27fb2060365
Alexandros Kosiaris <akosiaris@gmail.com>
2023-04-21 13:37:40 +0000

    Add a test for testing serialization/deserialization
    Nothing fancy, basic test. It doesn't even work yet

--- FuelEntry_test.ts
+++ FuelEntry_test.ts
@@ -14,7 +14,24 @@ Deno.test('Simple Correct Entry', () => {
     assertEquals(entry.unit, 'Κυβικό Μέτρο');
     assertEquals(entry.meanPrice, 100);
     assertEquals(entry.vatPrice, 124);
+});
+
+Deno.test('Test serialization of entry', () => {
+    const date = new Date('2020-01-01T00:00:00.000Z');
+    const entry = new FuelEntry(
+        date,
+        'Βενζίνες',
+        'τιμές σε €/m3, συμπεριλ. φόρων – τελών, προ ΦΠΑ',
+        'UNLEADED 100',
+        99,
+        101,
+    );
+    console.log(typeof(entry.date));
+    console.log(typeof(entry.meanPrice));
+    console.log(typeof(entry.category));
     const serialized: string = entry.serialize();
     const unserialized: FuelEntry = FuelEntry.unserialize(serialized);
     console.log(typeof(unserialized.date));
+    console.log(typeof(unserialized.meanPrice));
+    console.log(typeof(unserialized.category));
 });
