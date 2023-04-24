75eb1ed31d67acfd629441093ff0f011231e83b4
Alexandros Kosiaris <akosiaris@gmail.com>
2023-04-24 10:17:57 +0000

    Ship more patches as JSON patches
    Here we fix some things that consistently bug me, plus 1 inconsistency
    * en-dash to hyphen
    * Greek Ε in DIΕSEL to English E letter
    * Make sure all HEATING GASOIL winter product are the same
    
    Also ship a simple tool to patch the original data using a json patches
    file and write the output to a file. For now, JSON only. Ship also the
    resulting patched_fuels file. For now, undocumented

---
+++ utils/patch_all.ts
@@ -0,0 +1,26 @@
+// Patch all data using our json_patches.json
+
+import { JsonPatch } from "https://deno.land/x/json_patch@v0.1.1/mod.ts";
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
+
+const inputfile = Deno.args[0];
+const outfile = Deno.args[1];
+const patchfile = Deno.args[2];
+const entries = await readJSON(inputfile);
+const patches = await readJSON(patchfile);
+const jPatch = new JsonPatch();
+
+const newdata = [];
+for (const entry of entries) {
+    let p = entry;
+    for (const patch of patches) {
+        try {
+             p = jPatch.patch(p, patch);
+        } catch(_e) {
+             // Test failed, ignore
+        }
+    }
+    newdata.push(p);
+}
+
+await writeJSON(outfile, newdata, null, 2);
