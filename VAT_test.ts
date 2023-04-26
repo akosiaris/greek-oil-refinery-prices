be929f279454e200976cfe2f8917ef0b34fda994
Alexandros Kosiaris <akosiaris@gmail.com>
2023-04-26 11:59:37 +0300

    VAT_test_ts: Appease deno-lint
    Switch to const for various let variables that were not ever reassigned

--- VAT_test.ts
+++ VAT_test.ts
@@ -2,36 +2,36 @@ import { assertEquals } from './test_deps.ts';
 import { VAT } from './VAT.ts';
 
 Deno.test('18%', () => {
-    let rate = VAT.VATbyDate(new Date('2004-01-01'));
+    const rate = VAT.VATbyDate(new Date('2004-01-01'));
     assertEquals(rate, 0.18);
 });
 
 Deno.test('19%', () => {
-    let rate = VAT.VATbyDate(new Date('2008-01-01'));
+    const rate = VAT.VATbyDate(new Date('2008-01-01'));
     assertEquals(rate, 0.19);
 });
 
 Deno.test('21%', () => {
-    let rate = VAT.VATbyDate(new Date('2010-04-01'));
+    const rate = VAT.VATbyDate(new Date('2010-04-01'));
     assertEquals(rate, 0.21);
 });
 
 Deno.test('23%', () => {
-    let rate = VAT.VATbyDate(new Date('2010-10-01'));
+    const rate = VAT.VATbyDate(new Date('2010-10-01'));
     assertEquals(rate, 0.23);
 });
 
 Deno.test('24%', () => {
-    let rate = VAT.VATbyDate(new Date('2016-10-01'));
+    const rate = VAT.VATbyDate(new Date('2016-10-01'));
     assertEquals(rate, 0.24);
 });
 
 Deno.test('2019-05-04T00:00:00.000Z', () => {
-    let rate = VAT.VATbyDate(new Date('2019-05-04T00:00:00.000Z'));
+    const rate = VAT.VATbyDate(new Date('2019-05-04T00:00:00.000Z'));
     assertEquals(rate, 0.24);
 });
 
 Deno.test('No known VAT', () => {
-    let rate = VAT.VATbyDate(new Date('2000-01-01'));
+    const rate = VAT.VATbyDate(new Date('2000-01-01'));
     assertEquals(rate, NaN);
 })
