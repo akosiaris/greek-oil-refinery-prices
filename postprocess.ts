c2d2cdca437f1f17e2bd23ca6242ac91c57855be
Alexandros Kosiaris <akosiaris@gmail.com>
2023-05-01 08:11:36 +0000

    Put main into a deno if guard.
    This way we can execute main() if called directly and still be importable

--- postprocess.ts
+++ postprocess.ts
@@ -234,35 +234,41 @@ export async function writeDataFiles(data: FuelEntry[]): Promise<void> {
     appendSQLiteData(data, sqlitedatafile);
 }
 
-try {
-  const xmlfile: string = Deno.args[0]
-  const elasticsearch_url = env['ELASTICSEARCH_URL'];
-  const elasticsearch_username = env['ELASTICSEARCH_USERNAME']
-  const elasticsearch_password = env['ELASTICSEARCH_PASSWORD'];
-  const b64 = btoa(elasticsearch_username + ':' + elasticsearch_password);
+async function main(): Promise<void> {
+  try {
+    const xmlfile: string = Deno.args[0]
+    const elasticsearch_url = env['ELASTICSEARCH_URL'];
+    const elasticsearch_username = env['ELASTICSEARCH_USERNAME']
+    const elasticsearch_password = env['ELASTICSEARCH_PASSWORD'];
+    const b64 = btoa(elasticsearch_username + ':' + elasticsearch_password);
 
-  if (xmlfile) {
-    const xml: string = await readTXT(xmlfile);
-    const parsed: FuelEntry[] = await parseUnParsed(xml);
-    await writeDataFiles(parsed);
-    if (elasticsearch_url) {
-      /* Now, let's post them to elasticsearch */
-      for (const entry of parsed) {
-          const response = await fetch(elasticsearch_url, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Basic ' + b64,
-              },
-              body: JSON.stringify(entry),
-          });
-          if (response.status != 201) {
-            // POST failed, log why
-            console.log(response);
-          }
+    if (xmlfile) {
+      const xml: string = await readTXT(xmlfile);
+      const parsed: FuelEntry[] = await parseUnParsed(xml);
+      await writeDataFiles(parsed);
+      if (elasticsearch_url) {
+        /* Now, let's post them to elasticsearch */
+        for (const entry of parsed) {
+            const response = await fetch(elasticsearch_url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic ' + b64,
+                },
+                body: JSON.stringify(entry),
+            });
+            if (response.status != 201) {
+              // POST failed, log why
+              console.log(response);
+            }
+        }
       }
     }
+  } catch(error) {
+    console.log(error);
   }
-} catch(error) {
-  console.log(error);
+}
+
+if (import.meta.main) {
+  await main()
 }
