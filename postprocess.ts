27137d01aed462f0e574826d42c022828045479b
Alexandros Kosiaris <akosiaris@gmail.com>
2024-04-08 09:08:17 +0000

    Remove elasticsearch support from postprocess.ts
    It ended up being too fragile. Relying on SQLite files directly has
    proven to be more robust for my use case

--- postprocess.ts
+++ postprocess.ts
@@ -1,4 +1,3 @@
-import { env } from "node:process";
 import { FuelEntry } from "./src/FuelEntry.ts";
 
 import { writeDataFiles } from "./src/datastorage.ts";
@@ -10,32 +9,11 @@ const statefile = "state.json";
 async function main(): Promise<void> {
   try {
     const xmlfile: string = Deno.args[0];
-    const elasticsearch_url = env["ELASTICSEARCH_URL"];
-    const elasticsearch_username = env["ELASTICSEARCH_USERNAME"];
-    const elasticsearch_password = env["ELASTICSEARCH_PASSWORD"];
-    const b64 = btoa(elasticsearch_username + ":" + elasticsearch_password);
 
     if (xmlfile) {
       const xml: string = await readTXT(xmlfile);
       const parsed: FuelEntry[] = await parseUnParsed(xml, statefile);
       await writeDataFiles(parsed);
-      if (elasticsearch_url) {
-        /* Now, let's post them to elasticsearch */
-        for (const entry of parsed) {
-          const response = await fetch(elasticsearch_url, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": "Basic " + b64,
-            },
-            body: JSON.stringify(entry),
-          });
-          if (response.status != 201) {
-            // POST failed, log why
-            console.log(response);
-          }
-        }
-      }
     }
   } catch (error) {
     console.log(error);
